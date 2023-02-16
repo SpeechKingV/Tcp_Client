d1b580a46faf2e7e8a3d936fe6eaf006bd4aa446
NoizeBoy <speechking09@gmail.com>
2023-02-16 14:39:51 +0300

    no longer hangs when starting work

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MyClient Client("localhost", 2323);
+    qDebug();
+    MyClient Client("127.0.0.1", 2323);
+    qDebug();
     Client.show();
+    qDebug();
     return a.exec();
 }
+
--- myclient.cpp
+++ myclient.cpp
@@ -3,22 +3,26 @@
 MyClient::MyClient(const QString& strHost, const int nPort,QWidget *parent) : QWidget(parent)
                                                                         , m_nNextBlockSize(0)
 {
+    qDebug();
 
     m_pTcpSocket = new QTcpSocket(this);
 
     m_pTcpSocket->connectToHost(strHost,nPort);
     connect(m_pTcpSocket,SIGNAL(connected()),SLOT(SConnected()));
-    connect(m_pTcpSocket,SIGNAL(readyRead()),SLOT(SReadyRead()));
     connect(m_pTcpSocket,SIGNAL(error(QAbstractSocket::SockedError)),this,SLOT(QAbstractSocket::SockedError));
+
+    qDebug();
 
     m_ptxtInfo  = new QTextEdit;
     m_ptxtInput = new QLineEdit;
+
+    qDebug();
 
     m_ptxtInfo->setReadOnly(true);
 
     QPushButton* pcmd = new QPushButton;
     connect(pcmd,SIGNAL(clicked()),SLOT(SSendToServer()));
-    connect(m_ptxtInfo,SIGNAL(returnPresed()),this,SLOT(SSendToServer()));
+    connect(m_ptxtInfo,SIGNAL(returnPressed()),this,SLOT(SSendToServer()));
 
     //Layout setup
     QVBoxLayout* pvbxLayout = new QVBoxLayout;
@@ -26,6 +30,8 @@ MyClient::MyClient(const QString& strHost, const int nPort,QWidget *parent) : QW
     pvbxLayout->addWidget(m_ptxtInfo);
     pvbxLayout->addWidget(m_ptxtInput);
     pvbxLayout->addWidget(pcmd);
+
+    qDebug();
 
     setLayout(pvbxLayout);
 
@@ -39,21 +45,33 @@ void MyClient::SReadyRead()
 
     for (; ; )
     {
+        qDebug();
+
         if(!m_nNextBlockSize)
         {
+            qDebug();
+
             if(m_pTcpSocket->bytesAvailable() < sizeof (quint16))
             {
+                qDebug();
+
                 break;
             }
             in >> m_nNextBlockSize;
         }
 
+        qDebug();
+
         if(m_pTcpSocket->bytesAvailable() < m_nNextBlockSize)
         {
+            qDebug();
+
             QTime time;
             QString str;
 
             in >> time >> str;
+
+            qDebug();
 
             m_ptxtInfo->append(time.toString() + " " + str);
             m_nNextBlockSize = 0;
@@ -63,6 +81,8 @@ void MyClient::SReadyRead()
 
 void MyClient::SError(QAbstractSocket::SocketError err)
 {
+    qDebug();
+
     QString strError = "Error: " + (err == QAbstractSocket::HostNotFoundError ?
                                     "The host was not found." :
                                     err == QAbstractSocket::RemoteHostClosedError ?
@@ -71,25 +91,38 @@ void MyClient::SError(QAbstractSocket::SocketError err)
                                     "The connection was refused." :
                                     QString(m_pTcpSocket->errorString())
                                    );
+    qDebug();
+
+
     m_ptxtInfo->append(strError);
 }
 
 void MyClient::SSendToServer()
 {
+    qDebug();
+
     QByteArray arrBlock;
     QDataStream out(&arrBlock,QIODevice::WriteOnly);
-    out.setVersion(QDataStream::Qt_5_2);
+    out.setVersion(QDataStream::Qt_5_3);
     out << quint16(0) << QTime::currentTime() << m_ptxtInfo->toPlainText();
+
+    qDebug();
 
     out.device()->seek(0);
     out << quint16(arrBlock.size() - sizeof (quint16));
 
     m_pTcpSocket->write(arrBlock);
+    m_pTcpSocket->waitForBytesWritten(500);
+
+    qDebug();
+
     m_ptxtInfo->setText("");
 }
 
 void MyClient::SConnected()
 {
+    qDebug();
+
     m_ptxtInfo->append("Recived the connected() signal");
 }
 
